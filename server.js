5a44e7ba1ce5ef73bd49ffabeed7558ec4ce9189
elenabaurkot <baurkot@Baurkots-iMac.fios-router.home>
2020-05-15 01:27:54 -0400

    initial routes set up and postman hitting

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const connectDB = require('./config/db');
+const routes = require('./routes');
 
 const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Define Routes
+app.use(routes);
+
+// Connect Database
+connectDB();
 
 app.get('/', (req, res) => res.send('API Running'));
-
-const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
