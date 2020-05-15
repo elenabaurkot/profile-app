8b6311a36d51ed5b8c3edfe950e066ccd15e15e9
elenabaurkot <baurkot@Baurkots-iMac.fios-router.home>
2020-05-15 19:50:00 -0400

    Authorized and validated login route created

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -1,4 +1,8 @@
 const Users = require('../models/Users');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const { validationResult } = require('express-validator');
 
 const authorizeUser = async (req, res) => {
   try {
@@ -10,6 +14,51 @@ const authorizeUser = async (req, res) => {
   }
 };
 
+// register Users
+const loginUser = async (req, res) => {
+  //  sends error message for invalid entries to user registration
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const { email, password } = req.body;
+
+  try {
+    // See if user exists
+    let user = await Users.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ errors: [{ msg: 'Invalid Credentials' }] });
+    }
+
+    // check if password matches the user's stored password
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ errors: [{ msg: 'Invalid Credentials' }] });
+    }
+
+    // Return JSONWebToken
+    const payload = {
+      user: {
+        id: user.id,
+      },
+    };
+    jwt.sign(
+      payload,
+      config.get('jwtSecret'),
+      { expiresIn: 3600 },
+      (err, token) => {
+        if (err) throw err;
+        res.json({ token });
+      }
+    );
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+};
+
 module.exports = {
   authorizeUser,
+  loginUser,
 };
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,10 +1,26 @@
 const router = require('express').Router();
-const { authorizeUser } = require('../../controllers/auth-controller');
 const auth = require('../../middleware/auth');
+const { check } = require('express-validator');
+const {
+  authorizeUser,
+  loginUser,
+} = require('../../controllers/auth-controller');
 
 // @route       GET api/auth
-// @desc        Test route
+// @desc        Get user info after authorized
 // @access      Public
 router.route('/').get(auth, authorizeUser);
+
+// @route   POST api/auth
+// @desc    Authenticate user & get token
+// @access  Public
+router.route('/').post(
+  // Express validator checks to make sure required fields are entered correctly
+  [
+    check('email', 'Please include a valid email').isEmail(),
+    check('password', 'Password is required').exists(),
+  ],
+  loginUser
+);
 
 module.exports = router;
