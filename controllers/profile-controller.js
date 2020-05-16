eab0d0401af7d115a8c71c1680b66e16c015bbab
elenabaurkot <elenabaurkot@gmail.com>
2020-05-16 10:41:36 -0400

    create and update profile routes created

--- controllers/profile-controller.js
+++ controllers/profile-controller.js
@@ -1,6 +1,8 @@
 const Profile = require('../models/Profile');
 const Users = require('../models/Users');
+const { validationResult } = require('express-validator');
 
+// Get user's profile
 const getProfile = async (req, res) => {
   try {
     const profile = await Profile.findOne({
@@ -18,6 +20,72 @@ const getProfile = async (req, res) => {
   }
 };
 
+// Create or update user profile
+const createUpdateProfile = async (req, res) => {
+  const errors = validationResult(req);
+  if(!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array()})
+  }
+
+  const {
+    company,
+    website,
+    location,
+    bio,
+    status,
+    githubusername,
+    skills,
+    youtube,
+    facebook,
+    twitter,
+    instagram,
+    linkedin
+  } = req.body;
+
+  // Build profile object
+  const profileFields = {};
+  profileFields.user = req.user.id; 
+  if (company) profileFields.company = company; 
+  if (website) profileFields.website = website; 
+  if (location) profileFields.location = location;
+  if (bio) profileFields.bio = bio;
+  if (status) profileFields.status = status; 
+  if (githubusername) profileFields.githubusername = githubusername;
+  if (skills) {
+    profileFields.skills = skills.split(',').map(skill => skill.trim());
+  }
+
+  // Build social object
+  profileFields.social = {}; 
+  if (youtube) profileFields.social.youtube = youtube; 
+  if (twitter) profileFields.social.twitter = twitter; 
+  if (facebook) profileFields.social.facebook = facebook; 
+  if (linkedin) profileFields.social.linkedin = linkedin; 
+  if (instagram) profileFields.social.instagram = instagram; 
+
+  try {
+    let profile = await Profile.findOne({ user: req.user.id });
+    // Update profile
+    if(profile){
+      profile = await Profile.findOneAndUpdate(
+        { user: req.user.id }, 
+        { $set: profileFields}, 
+        { new: true }
+      );
+      return res.json(profile); 
+    }
+    // Create Profile
+    profile = new Profile(profileFields); 
+    await profile.save();
+    res.json(profile);
+
+  } catch(err) {
+    console.error(err.message);
+    res.status(500).send('Server Error'); 
+  }
+}
+
 module.exports = {
   getProfile,
+  createUpdateProfile
 };
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -2,11 +2,23 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
-const { getProfile } = require('../../controllers/profile-controller');
+const { getProfile, createUpdateProfile } = require('../../controllers/profile-controller');
+const { check } = require('express-validator');
 
 // @route       GET api/profile/me
 // @desc        Get current users profile
 // @access      Private
 router.route('/me').get(auth, getProfile);
+
+// @route       POST api/profile
+// @desc        Create or update user profile
+// @access      Private
+router.route('/').post(
+    [auth, [
+        check('status', 'Status is required').not().isEmpty(),
+        check('skills', 'Skills is required').not().isEmpty()
+    ]],
+    createUpdateProfile
+);
 
 module.exports = router;
